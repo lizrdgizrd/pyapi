50fdcc8b6a0648ddd74a271d0bbf485c10622af6
Rob Moffett <rob.moffett@gmail.com>
2019-08-05 08:04:35 -0400

    Last commit for class

---
+++ garbage.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+''' Some stuff '''
+
+def main():
+    ''' main is main '''
+    a_thing = "far away"
+
+    print "Over the hills"
+    print "and"
+    print "far away"
+
+    for all_the_things in a_thing:
+        print all_the_things
+
+main()
